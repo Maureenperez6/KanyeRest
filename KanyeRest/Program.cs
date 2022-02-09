01f62ed524f92fcbf36d2fca0b392562b8d07f91
Maureen Perez <maureen8694@gmail.com>
2022-02-09 12:47:12 -0500

    initial commit

--- KanyeRest/Program.cs
+++ KanyeRest/Program.cs
@@ -8,8 +8,36 @@ namespace KanyeRest
     {
         static void Main(string[] args)
         {
-            Quotes.KanyeQuote();
-            Quotes.RonQuote();
+            var client = new HttpClient();
+
+            var kanyeURL = "https://api.kanye.rest";
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+            var cont = true;
+
+            do
+            {
+                var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+
+                var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+                var ronResponse = client.GetStringAsync(ronURL).Result;
+
+                var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+
+                Console.WriteLine($"--------------------");
+                Console.WriteLine($"Kanye:{kanyeQuote}\n");
+                Console.WriteLine($"--------------------");
+
+                Console.WriteLine($"--------------------");
+                Console.WriteLine($"Ron:{ronQuote}\n");
+                Console.WriteLine($"--------------------");
+
+                Console.WriteLine("Continue ? Y or N?");
+                var response = Console.ReadLine().ToLower();
+                cont = (response == "n") ? false : true;
+            }while (cont);
+
         }
     }
 }
